3ac1a3b3fc8cc34573df578725828a2a05b18d80
Bazen Teklehaymanot <bazenteklehaymanot29@gmail.com>
2019-07-23 11:27:21 +0300

    api is set

--- SeekersOfTalent.Api/Program.cs
+++ SeekersOfTalent.Api/Program.cs
@@ -17,8 +17,22 @@ namespace SeekersOfTalent.Api
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("hosting.json", optional: true, reloadOnChange: true)
+              .Build();
+
+            return (
+                WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                );
+        }
     }
 }
